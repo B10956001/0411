d8ab9596ab6e7941920818ffb10497d0030fe94d
user <user@DESKTOP-6JBEF5K>
2022-04-11 10:19:00 +0800

    0.0.2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,10 +31,10 @@ namespace WindowsFormsApp1
         {
             this.button_connect = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_target_IP = new System.Windows.Forms.TextBox();
+            this.textBox_target_Port = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox_listen_Port = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -58,19 +58,19 @@ namespace WindowsFormsApp1
             this.label1.TabIndex = 1;
             this.label1.Text = "目標IP";
             // 
-            // textBox1
+            // textBox_target_IP
             // 
-            this.textBox1.Location = new System.Drawing.Point(125, 83);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 2;
+            this.textBox_target_IP.Location = new System.Drawing.Point(125, 83);
+            this.textBox_target_IP.Name = "textBox_target_IP";
+            this.textBox_target_IP.Size = new System.Drawing.Size(100, 22);
+            this.textBox_target_IP.TabIndex = 2;
             // 
-            // textBox2
+            // textBox_target_Port
             // 
-            this.textBox2.Location = new System.Drawing.Point(321, 84);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 4;
+            this.textBox_target_Port.Location = new System.Drawing.Point(321, 84);
+            this.textBox_target_Port.Name = "textBox_target_Port";
+            this.textBox_target_Port.Size = new System.Drawing.Size(100, 22);
+            this.textBox_target_Port.TabIndex = 4;
             // 
             // label2
             // 
@@ -82,12 +82,12 @@ namespace WindowsFormsApp1
             this.label2.TabIndex = 3;
             this.label2.Text = "目標Port";
             // 
-            // textBox3
+            // textBox_listen_Port
             // 
-            this.textBox3.Location = new System.Drawing.Point(518, 82);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 6;
+            this.textBox_listen_Port.Location = new System.Drawing.Point(518, 82);
+            this.textBox_listen_Port.Name = "textBox_listen_Port";
+            this.textBox_listen_Port.Size = new System.Drawing.Size(100, 22);
+            this.textBox_listen_Port.TabIndex = 6;
             // 
             // label3
             // 
@@ -104,16 +104,18 @@ namespace WindowsFormsApp1
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox_listen_Port);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox_target_Port);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_target_IP);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button_connect);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,10 +125,10 @@ namespace WindowsFormsApp1
 
         private System.Windows.Forms.Button button_connect;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_target_IP;
+        private System.Windows.Forms.TextBox textBox_target_Port;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox_listen_Port;
         private System.Windows.Forms.Label label3;
     }
 }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,19 +10,25 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using Microsoft.VisualBasic.PowerPacks;
+
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         UdpClient U;
         Thread Th;
+        ShapeContainer C; //畫布物件
+        ShapeContainer D; //畫布物件
+        Point stP; //繪圖起點
+        string p; //畫筆座標字串
         public Form1()
         {
             InitializeComponent();
         }
         private void Listen()
         {
-            
+
         }
         private void button_connect_Click(object sender, EventArgs e)
         {
@@ -43,6 +49,21 @@ namespace WindowsFormsApp1
             {
                 //忽略錯誤
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            C = new ShapeContainer(); //建立畫布
+            this.Controls.Add(C);
+            D = new ShapeContainer(); //建立畫布
+            this.Controls.Add(D);
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stP = e.Location;
+            p = stP.X.ToString() + "," + stP.Y.ToString();
         }
     }
 }
