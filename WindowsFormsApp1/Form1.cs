1f7ced38e3abcbb42532d2a0b9fc41f7f63d0d6b
user <user@DESKTOP-6JBEF5K>
2022-04-11 09:52:29 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        UdpClient U;
+        Thread Th;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Listen()
+        {
+            
+        }
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Th = new Thread(Listen);
+            Th.Start();
+            button_connect.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort(); //關閉監聽執行續
+                U.Close(); //關閉監聽器
+            }
+            catch
+            {
+                //忽略錯誤
+            }
+        }
+    }
+}
